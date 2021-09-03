a5ceae92f8a03b3fe6b3e6cd576c1309f70d9bbe
Ajil Abdalqader <ajilqado@gmail.com>
2021-09-03 14:30:05 +0200

    setting value of textarea and invoke setMarkdown onChange then desplay onChange in article section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react'
-import ReactMarkdown from 'react-markdown'
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 function App() {
-   const [markdown, setMarkdown] = useState('# markdown preview')
-  return <main>
-  <section className="markdown">
-  <textarea className="input"></textarea>
-  
-  </section>
-  </main>
+  const [markdown, setMarkdown] = useState("# markdownand");
+  return (
+    <main>
+      <section className="markdown">
+        <textarea
+          className="input"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article className="result">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  );
 }
 
-export default App
-
+export default App;
