32bcdb6689c466fcdee4845804bc9a7ffab57586
Ajil Abdalqader <ajilqado@gmail.com>
2021-09-03 14:22:01 +0200

    setting initial state value and styling global classes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 
 function App() {
-  return <h2>markdown project</h2>
+   const [markdown, setMarkdown] = useState('# markdown preview')
+  return <main>
+  <section className="markdown">
+  <textarea className="input"></textarea>
+  
+  </section>
+  </main>
 }
 
 export default App
